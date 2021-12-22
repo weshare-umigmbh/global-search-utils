d415d37ce1cb0ca0df3a75007832f5a92c095dc5
Philipp <philipp.schierz.ext@urban-mobility.io>
2021-12-22 10:12:17 +0100

    SRE-226: get rid of camelCase

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ dependencies = [
 ]
 
 packages = [
-    package for package in setuptools.find_packages() if package.startswith("globalSearch")
+    package for package in setuptools.find_packages() if package.startswith("globalsearch")
 ]
 
 # Determine which namespaces are needed.
-namespaces = ["globalSearch"]
-if "globalSearch.utils" in packages:
-    namespaces.append("globalSearch.utils")
+namespaces = ["globalsearch"]
+if "globalsearch.utils" in packages:
+    namespaces.append("globalsearch.utils")
 
-setuptools.setup(name='globalSearch-utils',
+setuptools.setup(name='globalsearch-utils',
                  version='0.1.0',
                  description='Python global search utilities',
                  author='Philipp Schierz',
