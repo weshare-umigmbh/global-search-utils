f46c010b093ec6ae54616498a8dd7a9559751eb2
Jannis Relakis <jannis@urban-mobility.io>
2022-07-20 18:57:37 +0300

    Refactoring

--- globalsearch/utils/optimistic_locking_utils.py
+++ globalsearch/utils/optimistic_locking_utils.py
@@ -1,8 +1,10 @@
 import flask
 from umi.utils import log
 
+
 def respond_with_optimistic_locking_error(entity, entity_name, max_retries_on_version_conflict):
-    log.simple_entry(f"Failed to create/update/delete {entity_name}: {entity} after {max_retries_on_version_conflict} retries.")
+    log.simple_entry(
+        f"Failed to create/update/delete {entity_name}: {entity} after {max_retries_on_version_conflict} retries.")
     flask.abort(flask.make_response(flask.jsonify(error={
         "code": "VersionConflict",
         "message": f"Service failed to consistently update the {entity_name} due to {max_retries_on_version_conflict} "
