a213d0dfa61951027351d335ee17b3e04796e0b3
Jannis Relakis <jannis@urban-mobility.io>
2022-07-22 11:03:04 +0300

    Making enqueue time filed configurable

--- globalsearch/utils/validation_utils.py
+++ globalsearch/utils/validation_utils.py
@@ -2,8 +2,8 @@ import flask
 from umi.utils import log
 
 
-def validate_enqueue_time(entity_update, persisted_enqueue_time):
-    new_enqueue_time = entity_update.get("enqueue_time", 0)
+def validate_enqueue_time(entity_update, persisted_enqueue_time, enqueue_time_field="enqueue_time"):
+    new_enqueue_time = entity_update.get(enqueue_time_field, 0)
     if persisted_enqueue_time >= new_enqueue_time:
         log.simple_entry(f"Attempt to update with an outdated enqueue time is rejected. "
                          f"Currently stored enqueue_time: {persisted_enqueue_time}, transaction_update: {entity_update}. "
