2c106e6b57f2d1070ecbde130171213e83860741
Philipp Schierz <91866729+PSchierz@users.noreply.github.com>
2022-10-17 16:04:07 +0200

    change attributre field name (#5)

--- globalsearch/utils/event_translation_util.py
+++ globalsearch/utils/event_translation_util.py
@@ -5,5 +5,5 @@ import json
 def get_event_data(event):
     envelope = json.loads(event.data.decode('utf-8'))
     resource = json.loads(base64.b64decode(envelope['message']['data']))
-    operation = envelope['message']['attributes']['type']
+    operation = envelope['message']['attributes']['eventType']
     return resource, operation
